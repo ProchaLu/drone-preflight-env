23c840e8642d6f0382b27f57388d2b61b337da00
Lukas Prochazka <prochazka.lu@gmail.com>
2025-03-05 18:44:44 +0100

    Remove timestampz

--- scripts/alpine-postgresql-setup-and-start.sh
+++ scripts/alpine-postgresql-setup-and-start.sh
@@ -2,38 +2,38 @@
 
 set -o errexit
 
-echo "[Postgres][$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Setting up PostgreSQL on Alpine Linux..."
+echo "Setting up PostgreSQL on Alpine Linux..."
 
 PGHOST=/postgres-volume/run/postgresql
 PGDATA="$PGHOST/data"
 
-echo "[Postgres][$(date -u +"%Y-%m-%dT%H:%M:%SZ")] === ENV_VARS_START ==="
+echo "=== ENV_VARS_START ==="
 echo "NEXTAUTH_URL=https://example.com"
 echo "NEXTAUTH_SECRET=supersecret"
 echo "CLOUDINARY_CLOUD_NAME=cloudname"
 echo "CLOUDINARY_API_KEY=apikey"
 echo "CLOUDINARY_API_SECRET=apisecret"
-echo "[Postgres][$(date -u +"%Y-%m-%dT%H:%M:%SZ")] === ENV_VARS_END ==="
+echo "=== ENV_VARS_END ==="
 
-echo "[Postgres][$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Adding exclusive data directory permissions..."
+echo "Adding exclusive data directory permissions..."
 chmod 0700 "$PGDATA"
 
-echo "[Postgres][$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Initializing database cluster..."
+echo "Initializing database cluster..."
 initdb -D "$PGDATA"
 
-echo "[Postgres][$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Prepending volume path to Unix Domain Socket path..."
+echo "Prepending volume path to Unix Domain Socket path..."
 sed -i "s/#unix_socket_directories = '\/run\/postgresql'/unix_socket_directories = '\/postgres-volume\/run\/postgresql'/g" "$PGDATA/postgresql.conf"
 
-echo "[Postgres][$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Enabling connections on all available IP interfaces..."
+echo "Enabling connections on all available IP interfaces..."
 echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
 
-echo "[Postgres][$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Starting PostgreSQL with pg_ctl..."
+echo "Starting PostgreSQL with pg_ctl..."
 pg_ctl start -D "$PGDATA" > /dev/null
 
-echo "[Postgres][$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Checking PostgreSQL status..."
+echo "Checking PostgreSQL status..."
 pg_ctl status -D "$PGDATA"
 
-echo "[Postgres][$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Creating database, user and schema..."
+echo "Creating database, user and schema..."
 psql -U postgres postgres << SQL
   CREATE DATABASE $PGDATABASE;
   CREATE USER $PGUSERNAME WITH ENCRYPTED PASSWORD '$PGPASSWORD';
@@ -42,4 +42,4 @@ psql -U postgres postgres << SQL
   CREATE SCHEMA $PGUSERNAME AUTHORIZATION $PGUSERNAME;
 SQL
 
-echo "[Postgres][$(date -u +"%Y-%m-%dT%H:%M:%SZ")] PostgreSQL setup complete."
+echo "PostgreSQL setup complete."
