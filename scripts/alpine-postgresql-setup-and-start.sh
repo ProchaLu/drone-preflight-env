f82cc3c3255e28af74585d8b99be90d167c9016d
Lukas Prochazka <prochazka.lu@gmail.com>
2025-03-06 10:25:21 +0100

    Update environment variable output format in PostgreSQL setup script

--- scripts/alpine-postgresql-setup-and-start.sh
+++ scripts/alpine-postgresql-setup-and-start.sh
@@ -8,13 +8,8 @@ PGHOST=/postgres-volume/run/postgresql
 PGDATA="$PGHOST/data"
 
 # If the project has more env variables then PGHOST, PGDATABASE, PGUSERNAME and PGPASSWORD, add them here with fake values between the start and end echo statements
-echo "ENVIRONMENT VARIABLES START"
-echo "NEXTAUTH_URL=https://example.com"
-echo "NEXTAUTH_SECRET=supersecret"
-echo "CLOUDINARY_CLOUD_NAME=cloudname"
-echo "CLOUDINARY_API_KEY=apikey"
-echo "CLOUDINARY_API_SECRET=apisecret"
-echo "ENVIRONMENT VARIABLES END"
+echo "Environment variables:"
+echo '{ "NEXTAUTH_URL": "https://myapp.com", "APP_SECRET_KEY": "supersecretkey", "CLOUDINARY_CLOUD_NAME": "cloudname", "CLOUDINARY_API_KEY": "apikey", "CLOUDINARY_API_SECRET": "apisecret}'
 
 echo "Adding exclusive data directory permissions..."
 chmod 0700 "$PGDATA"
@@ -29,7 +24,7 @@ echo "Enabling connections on all available IP interfaces..."
 echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
 
 echo "Starting PostgreSQL with pg_ctl..."
-pg_ctl start --pgdata="$PGDATA" > /dev/null
+pg_ctl start --pgdata="$PGDATA" > "/tmp/postgres_startup.log" 2>&1
 
 echo "Checking PostgreSQL status..."
 pg_ctl status -D "$PGDATA"
